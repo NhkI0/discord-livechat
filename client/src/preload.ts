2a896df464481a39f9613db57a0f58f5e1f7a2e7
Dopa <guengant.noe@gmail.com>
2026-01-05 10:35:09 +0100

    fix: cleaned version of the preload

--- client/src/preload.ts
+++ client/src/preload.ts
@@ -1,6 +1,4 @@
 import { contextBridge } from 'electron';
 
-// Expose any APIs needed by the renderer
 contextBridge.exposeInMainWorld('electronAPI', {
-  // Add any IPC methods here if needed
 });
